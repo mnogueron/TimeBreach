77444eab58309461a3b684c9245e4e638563365a
mnogueron <matthieu.nogueron@live.fr>
2016-09-24 14:57:17 +0200

    add new button for activable objects

---
+++ Assets/Scripts/CrateController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrateController : MonoBehaviour {
+
+    public GameObject button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        button.transform.position = new Vector3(transform.position.x, transform.position.y + 0.65f, transform.position.z);
+	}
+}
--- Assets/Scripts/MaskController.cs
+++ Assets/Scripts/MaskController.cs
@@ -3,16 +3,31 @@ using System.Collections;
 
 public class MaskController : MonoBehaviour {
 
-    public GameObject player;
+    public float amplitudeX = 0.1f;
+    public float amplitudeY = 0.1f;
+    public float omegaX = 8.0f;
+    public float omegaY = 10.0f;
 
-    // Use this for initialization
-    void Start () {
-	
-	}
+    public float baseScaleX = 8.0f;
+    public float baseScaleY = 8.0f;
+
+    private float index;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.currentWorld.CompareTo(GameManager.World.PRESENT) == 0)
+        {
+            index += Time.deltaTime;
+            float x = baseScaleX + amplitudeX * Mathf.Cos(omegaX * index);
+            float y = baseScaleY + amplitudeY * Mathf.Cos(omegaY * index);
+            transform.localScale = new Vector3(x, 0, y);
+        }
+    }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(GameManager.Player().transform.position.x, GameManager.Player().transform.position.y, transform.position.z);
     }
 }
