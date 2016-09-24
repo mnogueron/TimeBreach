d5ff21a80f37a59819285254d79cdc44953cc4fc
Reatha TITH <rea.tith@gmail.com>
2016-09-24 15:43:02 +0200

    "E" button follows the crate

---
+++ Assets/Scripts/Activable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Activable : MonoBehaviour {
+
+	public GameObject button;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Crate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crate : Activable {
+
+	public Transform crateColliderCheck;
+	public LayerMask playerLayer;
+	public float crateColliderRadius = 100f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool collision = Physics2D.OverlapCircle (crateColliderCheck.position, crateColliderRadius, playerLayer);
+		Debug.Log (collision);
+		if(collision){
+			if (!button.activeSelf) {
+				button.SetActive (true);
+			}
+			button.transform.position = new Vector3(transform.position.x, transform.position.y + 0.65f, transform.position.z);
+		}
+		else{
+			if (button.activeSelf) {
+				button.SetActive (false);
+			}	
+		}
+	}
+}
