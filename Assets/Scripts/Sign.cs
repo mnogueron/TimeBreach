c01cfa5f2a2d880503c77e40eacc65624cbd1a29
Reatha TITH <rea.tith@gmail.com>
2016-09-27 22:55:08 +0200

    Add signs and "help" messages

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sign : MonoBehaviour {
+
+	public GameObject signText;
+	public Transform signColliderCheck;
+	public float minDistance;
+
+	private string msgContent;
+
+	// Use this for initialization
+	void Start () {
+		signText.GetComponent<MeshRenderer> ().enabled = false;
+		msgContent = signText.GetComponent<TextMesh> ().text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distance = Vector2.Distance(signColliderCheck.position, Player.instance.transform.position);
+
+		if(distance < minDistance){
+			signText.GetComponent<MeshRenderer> ().enabled = true;
+		} else if(distance >= minDistance){
+			signText.GetComponent<MeshRenderer> ().enabled = false;
+		}
+	}
+		
+}
