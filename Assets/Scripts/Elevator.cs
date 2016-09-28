1654d9ba4ef6534689170476822742ac4caa91ec
Reatha TITH <rea.tith@gmail.com>
2016-09-28 21:23:51 +0200

    Implement lever and elevator

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+	private GameObject elevatorDown;
+	private GameObject elevatorUp;
+
+	// Use this for initialization
+	public void Start () {
+		elevatorDown = transform.FindChild ("elevator-down").gameObject;
+		elevatorUp = transform.FindChild ("elevator-up").gameObject;
+		elevatorDown.SetActive (true);
+		elevatorUp.SetActive (false);
+	}
+
+	public void Activate(){
+		elevatorDown.SetActive (false);
+		elevatorUp.SetActive (true);
+	}
+
+	public void Deactivate(){
+		elevatorDown.SetActive (true);
+		elevatorUp.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : Activable {
+
+	public Elevator elevator;
+
+	private GameObject leverInactive;
+	private GameObject leverActive;
+	private bool isActive;
+
+	public Lever() : base(){
+		;
+	}
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start ();
+
+		Debug.Log (transform.FindChild("LeverSprite"));
+
+		leverInactive = transform.FindChild("lever-inactive").gameObject;
+		leverActive = transform.FindChild("lever-active").gameObject;
+		leverInactive.SetActive (true);
+		leverActive.SetActive (false);
+		isActive = false;
+	}
+	
+	protected override void Activate(){
+		if (!isActive) {
+			// activate the lever
+			leverInactive.SetActive (false);
+			leverActive.SetActive (true);
+			elevator.Activate ();
+		} else {
+			// deactivate the lever
+			leverInactive.SetActive (true);
+			leverActive.SetActive (false);
+			elevator.Deactivate ();
+		}
+		isActive = !isActive;
+	}
+
+}
