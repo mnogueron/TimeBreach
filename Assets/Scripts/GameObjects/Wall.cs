557529582fdfe135bcc19a6c3f36db43f910d551
Reatha TITH <rea.tith@gmail.com>
2016-10-18 23:21:34 +0200

    Change some sign texts in Level 3

---
+++ Assets/Scripts/GameObjects/Wall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	public GameObject descriptionContainer;
+	public Transform wallColliderCheck;
+	public float minDistance;
+	public bool wallIsInFutureWorld;
+
+	// Use this for initialization
+	void Start () {
+		descriptionContainer.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
