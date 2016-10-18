286a44f6b72b230759e941294baf92d33417479b
Reatha TITH <rea.tith@gmail.com>
2016-10-18 22:14:24 +0200

    Remove useless sign texts and add some decoration to Level 2

--- Assets/Scripts/GameObjects/Collectable/Orb.cs
+++ Assets/Scripts/GameObjects/Collectable/Orb.cs
@@ -3,31 +3,10 @@ using System.Collections;
 
 public class Orb : Collectable {
 
-	public GameObject orbText;
-	public Transform orbColliderCheck;
-	public float minDistance;
-
-	// Use this for initialization
-	void Start () {
-		orbText.GetComponent<MeshRenderer> ().enabled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float distance = Vector2.Distance(orbColliderCheck.position, Player.instance.transform.position);
-
-		if(distance < minDistance){
-			orbText.GetComponent<MeshRenderer> ().enabled = true;
-		} else if(distance >= minDistance){
-			orbText.GetComponent<MeshRenderer> ().enabled = false;
-		}
-	}
-
 	protected override void PickUp()
 	{
         Debug.Log("inside pickup");
 		gameObject.SetActive(false);
-        orbText.SetActive(false);
 		Player.SetHasOrbem (true);
 		Debug.Log (Player.HasOrbem ());
 		UIManager.ShowPowerBar ();
