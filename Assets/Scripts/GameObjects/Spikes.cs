28d2a3519dccfdb7070e115666b4b715e2070f81
Reatha TITH <rea.tith@gmail.com>
2016-10-20 00:06:29 +0200

    Change spikes in Level 2. Now using prefabs instead of creating them with Tiled

---
+++ Assets/Scripts/GameObjects/Spikes.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D hit){
+		if(hit.tag == "Player"){
+			StartCoroutine (SceneController.ResetScene ());
+		}
+	}
+}
